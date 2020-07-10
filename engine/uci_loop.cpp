93e012fbbeb4f2edaaedf215fcc135c592bb5bb2
Lukasz Szpakowski <luckboy@vp.pl>
2020-07-10 20:54:58 +0200

    Added inclusion.

--- engine/uci_loop.cpp
+++ engine/uci_loop.cpp
@@ -16,6 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <iostream>
+#include <limits>
 #include <sstream>
 #include <string>
 #include <unordered_map>
