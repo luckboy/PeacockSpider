2e61c84f502358a2067b828cac44516615ce2764
Lukasz Szpakowski <luckboy@vp.pl>
2020-10-12 09:37:00 +0200

    Added fitness function class.

---
+++ genalg/fitness.hpp
@@ -0,0 +1,54 @@
+/*
+ * Peacock Spider - Chess engine.
+ * Copyright (C) 2020 Łukasz Szpakowski
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _FITNESS_HPP
+#define _FITNESS_HPP
+
+#include "tournament.hpp"
+
+namespace peacockspider
+{
+  namespace genalg
+  {
+    class FitnessFunction
+    {
+      Tournament *_M_tournament;
+      std::function<void (const TournamentResult &)> _M_fitness_output_function;
+    public:
+      FitnessFunction(Tournament *tournament);
+
+      ~FitnessFunction();
+
+      std::function<void (int, int, int, int, Result)> tournament_output_function() const;
+
+      void set_tournament_output_function(std::function<void (int, int, int, int, Result)> fun);
+
+      std::function<void (const TournamentResult &)> fitness_output_function() const;
+
+      void set_fitness_output_function(std::function<void (const TournamentResult &)> fun);
+
+      int individual_count() const
+      { return _M_tournament->result().player_count(); }
+
+      bool update(int iter, const std::vector<std::shared_ptr<int []>> &gene_arrays);
+
+      int operator()(int i) const;
+    };
+  }
+}
+
+#endif
---
+++ genalg/fitness_fun.cpp
@@ -0,0 +1,53 @@
+/*
+ * Peacock Spider - Chess engine.
+ * Copyright (C) 2020 Łukasz Szpakowski
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "fitness.hpp"
+
+using namespace std;
+
+namespace peacockspider
+{
+  namespace genalg
+  {
+    FitnessFunction::FitnessFunction(Tournament *tournament) :
+      _M_tournament(tournament), _M_fitness_output_function([](const TournamentResult &result) {}) {}
+
+    FitnessFunction::~FitnessFunction() {}
+
+    function<void (int, int, int, int, Result)> FitnessFunction::tournament_output_function() const
+    { return _M_tournament->tournament_output_function(); }
+
+    void FitnessFunction::set_tournament_output_function(std::function<void (int, int, int, int, Result)> fun)
+    { _M_tournament->set_tournament_output_function(fun); }
+
+    function<void (const TournamentResult &)> FitnessFunction::fitness_output_function() const
+    { return _M_fitness_output_function; }
+
+    void FitnessFunction::set_fitness_output_function(function<void (const TournamentResult &)> fun)
+    { _M_fitness_output_function = fun; }
+
+    bool FitnessFunction::update(int iter, const vector<shared_ptr<int []>> &gene_arrays)
+    {
+      if(!_M_tournament->play(iter, gene_arrays)) return false;
+      _M_fitness_output_function(_M_tournament->result());
+      return true;
+    }
+
+    int FitnessFunction::operator()(int i) const
+    { return _M_tournament->result().score(i); }
+  }
+}
