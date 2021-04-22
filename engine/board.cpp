6b4b9094f65fa45e606e3864908d73c2184e4976
Lukasz Szpakowski <luckboy@vp.pl>
2021-04-22 20:14:33 +0200

    Fixed copyright years.

--- engine/board.cpp
+++ engine/board.cpp
@@ -1,6 +1,6 @@
 /*
  * Peacock Spider - Chess engine.
- * Copyright (C) 2020 Łukasz Szpakowski
+ * Copyright (C) 2020-2021 Łukasz Szpakowski
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
